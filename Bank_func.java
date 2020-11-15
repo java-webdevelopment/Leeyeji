192d97715ecc3b6e9a726cbdcace105e0afab6f8
hey880 <67099741+hey880@users.noreply.github.com>
2020-11-15 11:33:57 +0900

    Update Bank_func.java

--- Bank_func.java
+++ Bank_func.java
@@ -1,16 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Bank_func{
 
-	Scanner scan = new Scanner(System.in); //입,출금 기능에서 금액을 입력 받는 
+	private String id;
+	private String pw;
+	private int balance;//잔고 저장
+	private int money;//입력 받은 금액 저장
+	ArrayList<String> pre = new ArrayList<String>();//직전 동작
 
-	private int balance;
-	private int money;
-	private String pre;
+	public String getId() {
+		return id;
+	}
 
-	public void menu(String id) {		
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+
+	public void login() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Insert your Id:");
+		this.id = scan.nextLine();
+
+		System.out.print("Insert your password:");
+		this.pw = scan.nextLine();
+	}
+
+	public void menu() {		
 		System.out.println("Welcome XYZ");
-		System.out.println("Your ID is "+id);
+		System.out.println("Your ID is "+this.id);
 		System.out.println();
 		System.out.println("A. Check Balance");
 		System.out.println("B. Deposit");
@@ -21,66 +48,70 @@ class Bank_func{
 
 	public void balance() {
 		System.out.println();
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
 		System.out.println("Balance = "+balance);
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
 		System.out.println();
 	}
 
 	public void deposit() {
 		System.out.println();
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
 		System.out.println("Enter an amount to deposit");
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
+
+		Scanner scan = new Scanner(System.in);
+		//입금, 출금 기능에서 금액 입력 받는 스캐너
+
 		this.money = Integer.parseInt(scan.nextLine());
 
 		if (money<=0) {
 			System.out.println("0원 이상 입력하세요.");
 		}else {			
 			balance+=money;
-			pre = "Deposited: ";
+			pre.add("Deposited: "+money);
 		}
 
 		System.out.println();
+
 	}
 
 	public void withdraw() {
 		System.out.println();
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
 		System.out.println("Enter an amount to withdraw");
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
+
+		Scanner scan = new Scanner(System.in);
+		//입금, 출금 기능에서 금액 입력 받는 스캐너
+
 		this.money = Integer.parseInt(scan.nextLine());
 
 		if(money>balance) {
 			System.out.println("현재 잔고는 "+balance+"원 입니다.");
 			System.out.println(balance+"원 이상 출금할 수 없습니다.");
+			pre.add("잔고부족으로 출금하지 못했습니다.");
 		}else if(money<=0){
 			System.out.println("0원 이상 입력하세요.");
 		}else {
 			balance-=money;
+			pre.add("withdrawn: "+money);			
 		}
-		pre = "withdrawn: ";			
 
 		System.out.println();
+
 	}
 
 	public void pre() {
 		System.out.println();
-		System.out.println("-------------------");			
+		System.out.println("----------------------------");
 
-		if(pre==null) {
+		if(pre.isEmpty()) {
 			System.out.println("아무동작도 하지 않았습니다!");
-		}else if(balance==0) {
-			//예를 들어 잔고가 2000원이었는데 withdraw의 money에 2000을 입력하면
-			//잔고가 0이 되므로 money>balance에 걸려 아래의 출금에 실패 했다는 메시지 출력.
-			//이를 방지 하기 위해 balance가 0이 될 경우 'withdraw : 금액'이 출력 되도록 함.
-			System.out.println(pre+money);
-		}else if(money>balance) {
-			System.out.println("잔고보다 금액이 많아 출금 실패하였습니다.");
 		}else {
-			System.out.println(pre+money);
+			System.out.println(pre.get(pre.size()-1));
 		}
-		System.out.println("-------------------");
+		System.out.println("----------------------------");
 		System.out.println();
 	}
 }
